f7819ba31c7ef8f9492b8832c83e46eb9019ca99
irakli-tkemaladze <tkemaladze96@gmail.com>
2024-03-22 13:35:46 +0400

    fix-homework-3

---
+++ meeting-3/homework-1/3-1-homework-solution.js
@@ -0,0 +1,80 @@
+class Vechile {
+  constructor(make, model) {
+    this.make = make;
+    this.model = model;
+  }
+}
+class Car extends Vechile {
+  constructor(make, model, year) {
+    super(make, model)
+    this.year = year;
+   
+  }
+  owners = [];
+  removeOwner = function (person) { this.owners = this.owners.filter(owner => owner != person) };
+  addOwner = function (person) { this.owners.push(person) };
+  getCarInfo = function () { return `${this.make} ${this.model} released in ${this.year}` };
+  getOwnersCount = function () { return this.owners.length };
+  getOwnerNames = function () {
+    let names = [];
+    this.owners.forEach(item => names.push(item.fullName()));
+    return names
+  };
+  getFullInfo = function () { return `${this.make} ${this.model} from ${this.year}. ${this.getOwnersCount()} person owns this car. These are - ${this.getOwnerNames().join(', ')}.` };
+}
+
+class Person {
+  constructor(name, surname, age, gender, cars = []) {
+    this.name = name;
+    this.surname = surname;
+    this.age = age;
+    this.gender = gender;
+    this.cars = cars;
+  }
+  fullName = function () { return `${this.name} ${this.surname}` };
+  countCars = function () { return this.cars.length };
+  buysCar = function (car) { this.cars.push(car); car.addOwner(this) };
+  sellsCar = function (car) { this.cars = this.cars.filter(item => item != car); car.removeOwner(this) };
+  getAllCarsInfo = function () {
+    return `${this.name} owns these cars: ${this.cars.map(carsFullInfo => carsFullInfo.getCarInfo()).join(', ')}.`;
+  }
+
+}
+
+module.exports = { Person, Car };
+// let daniel916 = new Person("Daniel", "Barbakadze", 21, "M", []);
+// let ilona = new Person("Elon", "Musk", 30, "M");
+// let duti_picoti = new Car("BMW", "525", "1999");
+// let stodevianosto = new Car("Mercedes", "E190", 1991);
+
+// daniel916.buysCar(duti_picoti); // adds passed car
+// daniel916.buysCar(stodevianosto); // adds passed car
+// daniel916.sellsCar(duti_picoti); // removes passed car
+// ilona.buysCar(stodevianosto); // adds passed car
+// ilona.buysCar(duti_picoti); // adds passed car
+
+// console.log({
+//   daniel: {
+//     fullName: daniel916.fullName(),
+//     countCars: daniel916.countCars(),
+//     getAllCarsInfo: daniel916.getAllCarsInfo(),
+//   },
+//   elon: {
+//     fullName: ilona.fullName(),
+//     countCars: ilona.countCars(),
+//     getAllCarsInfo: ilona.getAllCarsInfo(),
+//   },
+//   duti_picoti: {
+//     getOwnersCount: duti_picoti.getOwnersCount(),
+//     getOwnerNames: duti_picoti.getOwnerNames(),
+//     getFullInfo: duti_picoti.getFullInfo(),
+//     getCarInfo: duti_picoti.getCarInfo(),
+//   },
+//   stodevianosto: {
+//     getOwnersCount: stodevianosto.getOwnersCount(),
+//     getOwnerNames: stodevianosto.getOwnerNames(),
+//     getFullInfo: stodevianosto.getFullInfo(),
+//     getCarInfo: stodevianosto.getCarInfo(),
+//   },
+// });
+
